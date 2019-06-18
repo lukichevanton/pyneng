8990b4af9bde206040ea502ecadbfc91d5df0fa9
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2019-06-18 09:50:59 +0300

    Update task_5_1.py

--- 05_basic_scripts/task_5_1.py
+++ 05_basic_scripts/task_5_1.py
@@ -21,6 +21,8 @@ $ python task_5_1.py
 То есть эту задачу можно решить без использования условия if.
 '''
 
+#!/usr/bin/env python3
+
 london_co = {
     'r1': {
         'location': '21 New Globe Walk',
@@ -46,8 +48,6 @@ london_co = {
         'routing': True
     }
 }
-
-#!/usr/bin/env python3
 
 name = input("Enter device name: ")
 
