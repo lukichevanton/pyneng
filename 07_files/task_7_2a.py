578d1c2053feaaba87cc843e67fc717f88987d48
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2019-07-06 23:57:44 +0300

    Update task_7_2a.py

--- 07_files/task_7_2a.py
+++ 07_files/task_7_2a.py
@@ -12,4 +12,51 @@
 
 '''
 
+#!/usr/bin/env python3
+
+"""1-ый вариант"""
+f = open('config_sw1.txt')
+RESULT = f.read().split('\n')
+
 ignore = ['duplex', 'alias', 'Current configuration']
+
+for command in RESULT:
+    if command.startswith('!'):
+        continue
+    for item in ignore:
+            if item in command:
+                break
+    else:
+        print(' {}'.format(command))
+
+"""2-ой вариант"""
+ignore = ['duplex', 'alias', 'Current configuration']
+current_list = list()
+
+with open('config_sw1.txt', 'r') as f:
+    for line in f:
+        line = line.rstrip()
+
+        # Если пустая строка
+        if not line:
+            continue
+
+        # Если начинается на '!'
+        if line[0] == '!':
+            continue
+
+        # Если в строке есть строки из списка игнора
+        found_ignore = False
+        for item in ignore:
+            if item in line:
+                found_ignore = True
+                break
+
+        if found_ignore:
+            continue
+
+        current_list.append(line)
+
+
+for line in current_list:
+    print(line)
