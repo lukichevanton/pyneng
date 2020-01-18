5ebebdc9eea774dfbc16207897d64aff8f265a66
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-01-18 15:35:04 +0300

    Update task_4_6.py

--- 04_data_structures/task_4_6.py
+++ 04_data_structures/task_4_6.py
@@ -16,11 +16,10 @@ Outbound Interface     FastEthernet0/0
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
 
-ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h FastEthernet0/0'
-ospf_route = ospf_route.replace('O','OSPF').replace(',','').split()
-print("{:15} {:15}".format('Protocol:', ospf_route[0]))
-print("{:15} {:15}".format('Prefix:', ospf_route[1]))
-print("{:15} {:15}".format('AD/Metric:', ospf_route[2]))
-print("{:15} {:15}".format('Next-Hop:', ospf_route[4]))
-print("{:15} {:15}".format('Last update:', ospf_route[5]))
-print("{:15} {:15}".format('Outbound Interface:', ospf_route[6]))
+ospf_route = ospf_route.replace('O','OSPF').replace(',',' ').split()
+print("{:20} {:15}".format('Protocol:', ospf_route[0]))
+print("{:20} {:15}".format('Prefix:', ospf_route[1]))
+print("{:20} {:15}".format('AD/Metric:', ospf_route[2]))
+print("{:20} {:15}".format('Next-Hop:', ospf_route[4]))
+print("{:20} {:15}".format('Last update:', ospf_route[5]))
+print("{:20} {:15}".format('Outbound Interface:', ospf_route[6]))
