560d53447d06a5efa8c1bc5f6f194693f53243ea
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-01-18 15:20:43 +0300

    Update task_4_5.py

--- 04_data_structures/task_4_5.py
+++ 04_data_structures/task_4_5.py
@@ -14,49 +14,8 @@
 command1 = 'switchport trunk allowed vlan 1,2,3,5,8'
 command2 = 'switchport trunk allowed vlan 1,3,8,9'
 
-In [48]: command1 = 'switchport trunk allowed vlan 1,2,3,5,8'
-
-In [49]: command2 = 'switchport trunk allowed vlan 1,3,8,9'
-
-In [50]: vlans1 = command1.split()
-
-In [51]: vlans2 = command2.split()
-
-In [52]: vlans1 = vlans1[4].split(',')
-
-In [53]: vlans2 = vlans2[4].split(',')
-
-In [54]: vlans1 = set(vlans1)
-
-In [55]: vlans2 = set(vlans2)
-
-In [60]: Total = vlans1 & vlans2
-
-In [64]: Total = list(Total)
-
-In [68]: print(Total)
-['1', '3', '8']
-
-
-""""""
-
-
-In [100]: command1 = 'switchport trunk allowed vlan 1,2,3,5,8'
-
-In [101]: command2 = 'switchport trunk allowed vlan 1,3,8,9'
-
-In [102]: vlans = list(set((command1.split())[-1].split(',')) & set((command2.sp
-     ...: lit())[-1].split(',')))
-
-In [103]: print(vlans)
-['1', '3', '8']
-
-
-""""""
-
-
-command1 = 'switchport trunk allowed vlan 1,2,3,5,8'.split()
-command2 = 'switchport trunk allowed vlan 1,3,8,9'.split()
+command1 = command1.split()
+command2 = command2.split()
 vlans = list(set(command1[-1].split(',')).intersection(set(command2[-1].split(','))))
 print(vlans)
 
