2eb15ea6ca7d84409f0fc1095430f04b25696345
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-01-25 17:00:18 +0300

    Not completed

--- 05_basic_scripts/task_5_2b.py
+++ 05_basic_scripts/task_5_2b.py
@@ -13,7 +13,7 @@
 #!/usr/bin/env python3
 
 from sys import argv
-ip = arg[1:]
+ip, mask = arg[1:]
 
 ip,mask = (ip.split('/'))
 ip1,ip2,ip3,ip4 = (ip.split('.'))
@@ -25,6 +25,9 @@ print('{:<010} {:<010} {:<010} {:<010}'.format('{:b}'.format(int(ip1)) , '{:b}'.
 
 print('\n')
 print('Mask:')
+print('/',mask)
+
+#Далее непонятно как работает
 
 mask_bin = ('1' * int(mask))
 whilelen(mask_bin) < 32:
@@ -33,3 +36,5 @@ m1,m2,m3,m4 = mask_bin[0:8],mask_bin[8:16],mask_bin[16:24],mask_bin[24:32]
 
 print('{:<10} {:<10} {:<10} {:<10}'.format((int(m1,2)) , (int(m2,2)) , (int(m3,2)) , (int(m4,2))))
 print('{:<010} {:<010} {:<010} {:<010}'.format(m1,m2,m3,m4))
+
+"""Not completed"""
