d3237c6cd46db201bf343d335cec56700c98787d
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-01-26 15:04:42 +0300

    Update task_5_3*.py

--- 05_basic_scripts/task_5_3*.py
+++ 05_basic_scripts/task_5_3*.py
@@ -22,5 +22,23 @@ print('interface {}'.format(inter))
 print('\n'.join(template[mode]).format(vlan))
 
 Задание 5.3a
+mode = input('Введите режим работы интерфейса (access/trunk): ')
+inter = input('Введите тип и номер интерфейса (Fa0/1): ')
 
+vl  = {'access' : 'Введите номер VLAN: ', 'trunk' : 'Введите разрешенные VLANы: '}
+vlan = input(vl[mode])
+
+access_template = [
+    'switchport mode access', 'switchport access vlan {}',
+    'switchport nonegotiate', 'spanning-tree portfast',
+    'spanning-tree bpduguard enable'
+]
+trunk_template = [
+    'switchport trunk encapsulation dot1q', 'switchport mode trunk',
+    'switchport trunk allowed vlan {}'
+]
+template = {'access' : access_template, 'trunk' : trunk_template}
+
+print('interface {}'.format(inter))
+print('\n'.join(template[mode]).format(vlan))
 
