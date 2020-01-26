a2c62ac002c418857387e0d077f5bc3b2df118cc
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-01-26 15:10:04 +0300

    Update task_4_6.py

--- 04_data_structures/task_4_6.py
+++ 04_data_structures/task_4_6.py
@@ -14,6 +14,8 @@ Outbound Interface     FastEthernet0/0
 
 '''
 
+#!/usr/bin/env python3
+
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
 
 ospf_route = ospf_route.replace('O','OSPF').replace(',',' ').split()
