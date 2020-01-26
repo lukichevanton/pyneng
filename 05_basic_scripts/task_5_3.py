97e9b47dddbc8d822e6be87e06bef5e49c1edd9a
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-01-26 15:03:29 +0300

    Update task_5_3.py

--- 05_basic_scripts/task_5_3.py
+++ 05_basic_scripts/task_5_3.py
@@ -45,17 +45,6 @@ switchport mode trunk
 switchport trunk allowed vlan 2,3,4,5
 '''
 
-access_template = [
-    'switchport mode access', 'switchport access vlan {}',
-    'switchport nonegotiate', 'spanning-tree portfast',
-    'spanning-tree bpduguard enable'
-]
-
-trunk_template = [
-    'switchport trunk encapsulation dot1q', 'switchport mode trunk',
-    'switchport trunk allowed vlan {}'
-]
-
 #!/usr/bin/env python3
 
 mode = input('Введите режим работы интерфейса (access/trunk): ')
