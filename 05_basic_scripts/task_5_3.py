69d3737ba0a7b3ee7365ffab3185b064d4df8ec5
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-02-01 15:12:14 +0300

    Update task_5_3.py

--- 05_basic_scripts/task_5_3.py
+++ 05_basic_scripts/task_5_3.py
@@ -47,14 +47,15 @@ switchport trunk allowed vlan 2,3,4,5
 
 #!/usr/bin/env python3
 
+"""1ый вариант"""
 mode = input('Введите режим работы интерфейса (access/trunk): ')
 inter = input('Введите тип и номер интерфейса (Fa0/1): ')
 
 template2 = {
-             'access': 'Enter VLAN number:',
-             'trunk': 'Enter allowed VLANs:'}
+             'access': 'Введите номер VLAN:',
+             'trunk': 'Введите разрешенные VLANы:'}
              
-vlan = input('{}'. format(template2[mode]  ))
+vlan = input('{}'. format(template2[mode]))
 
 access_template = [
     'switchport mode access', 'switchport access vlan {}',
@@ -70,6 +71,35 @@ trunk_template = [
 template = {'access': access_template, 
              'trunk': trunk_template
              }
+
+print('interface {}'.format(inter))
+print('\n'.join(template[mode]).format(vlan))
+
+
+"""2ой вариант"""
+access_template = [
+    'switchport mode access', 'switchport access vlan {}',
+    'switchport nonegotiate', 'spanning-tree portfast',
+    'spanning-tree bpduguard enable'
+]
+
+trunk_template = [
+    'switchport trunk encapsulation dot1q', 'switchport mode trunk',
+    'switchport trunk allowed vlan {}'
+]
+
+template = {'access': access_template, 
+             'trunk': trunk_template,
+             'vlanaccess': 'Введите номер VLAN:',
+             'vlantrunk': 'Введите разрешенные VLANы:'
+             }
+
+mode = input('Введите режим работы интерфейса (access/trunk): ')
+inter = input('Введите тип и номер интерфейса (Fa0/1): ')
+
+mode_vlan = 'vlan' + mode
+
+vlan = input('{}'. format(template[mode_vlan]))
 
 print('interface {}'.format(inter))
 print('\n'.join(template[mode]).format(vlan))
