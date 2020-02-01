6f74168c44fb9a9ae1ee7192b39dbde6f7184748
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-02-01 15:41:16 +0300

    Update task_6_2a.py

--- 06_control_structures/task_6_2a.py
+++ 06_control_structures/task_6_2a.py
@@ -48,17 +48,13 @@ else:
     check_range = [byte for byte in ip if 0 <= byte <= 255]
     if not len(check_range) == 4:
         print('Incorrect IPv4 address')
-    elif 0 <= ip[0] <= 127:
-        print(('ip {} сlass A').format(ip_address))
-    elif 128 <= ip[0] <= 191:
-        print(('ip {} сlass B').format(ip_address))
-    elif 192 <= ip[0] <= 223:
-        print(('ip {} сlass C').format(ip_address))
-    elif 224 <= ip[0] <= 239:
-        print(('ip {} сlass D').format(ip_address))
+    elif ip[0] >= 1 and ip[0] <= 223:
+            print('unicast')
+    elif ip[0] >= 223 and ip[0] <= 239:
+            print('multicast')
     elif ip_address == '255.255.255.255':
-        print(('ip {} local broadcast').format(ip_address))
+        print("local broadcast")
     elif ip_address == '0.0.0.0':
-        print(('ip {} unassigned').format(ip_address))
+        print("unassigned")
     else:
         print('unused')
