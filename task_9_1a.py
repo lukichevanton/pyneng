73ce46b6cac6f2d9680dbaed3250aaaa62146c37
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-02-19 23:18:28 +0300

    Update task_9_1a.py

--- task_9_1a.py
+++ task_9_1a.py
@@ -1,5 +1,3 @@
-
-
 Задание 9.1a
 
 Сделать копию функции из задания 9.1.
@@ -40,7 +38,7 @@ def generate_access_config(intf_vlan_mapping, access_template,psecurity=None):#�
     template = []
     for intf, vlan in intf_vlan_mapping.items():
         template.append('interface ' + intf)
-        for line in access_mode_template:
+        for line in access_template:
             if line.endswith('access vlan'):
                 template.append(f'{line} {vlan}')
             else:
