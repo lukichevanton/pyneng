33a345ad9e2cd0fcf4c865bf224d3e0a859dc5ad
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-02-19 23:17:51 +0300

    Update task_9_1.py

--- task_9_1.py
+++ task_9_1.py
@@ -49,7 +49,7 @@ def generate_access_config(intf_vlan_mapping, access_template):
     template = []
     for intf, vlan in intf_vlan_mapping.items():
         template.append('interface ' + intf)
-        for line in access_mode_template:
+        for line in access_template:
             if line.endswith('access vlan'):
                 template.append(f'{line} {vlan}')
             else:
