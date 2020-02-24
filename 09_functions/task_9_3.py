de83aabbf4fef84bc695ffafc9786ed2efc3bf76
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-02-24 21:42:39 +0300

    Update task_9_3.py

--- 09_functions/task_9_3.py
+++ 09_functions/task_9_3.py
@@ -22,3 +22,26 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
+#!/usr/bin/env python3
+
+def get_int_vlan_map(config_filename):   
+    result = {}
+    result2 = {} 
+    with open(config_filename) as f:
+        for line in f:
+            if 'interface' in line:
+                interface = line.split()[1]
+            elif 'access vlan' in line:
+                access = line.split()[-1]
+                result[interface] = access
+            elif 'trunk allowed vlan' in line:
+                trunk = line.split()[-1]
+                result2[interface] = trunk
+    print(result)
+    print(result2)
+    return(result)
+    return(result2)
+get_int_vlan_map('config_sw1.txt')
+
+{'FastEthernet0/0': '10', 'FastEthernet0/2': '20', 'FastEthernet1/0': '20', 'FastEthernet1/1': '30'}
+{'FastEthernet0/1': '100,200', 'FastEthernet0/3': '100,300,400,500,600','FastEthernet1/2': '400,500,600'}
