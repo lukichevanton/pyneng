a30779a2491614f3398dc99309d8caff03f88842
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-02-24 21:41:15 +0300

    Update task_9_1a.py

--- 09_functions/task_9_1a.py
+++ 09_functions/task_9_1a.py
@@ -22,24 +22,6 @@
 
 '''
 
-access_mode_template = [
-    'switchport mode access', 'switchport access vlan',
-    'switchport nonegotiate', 'spanning-tree portfast',
-    'spanning-tree bpduguard enable'
-]
-
-port_security_template = [
-    'switchport port-security maximum 2',
-    'switchport port-security violation restrict',
-    'switchport port-security'
-]
-
-access_config = {
-    'FastEthernet0/12': 10,
-    'FastEthernet0/14': 11,
-    'FastEthernet0/16': 17
-}
-
 #!/usr/bin/env python3
 
 access_mode_template = [
