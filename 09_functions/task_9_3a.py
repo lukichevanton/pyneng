a37e676d222dc338948b0310251fcfa6a8d0c5a1
lukichevanton <50456448+lukichevanton@users.noreply.github.com>
2020-02-24 21:43:28 +0300

    Update task_9_3a.py

--- 09_functions/task_9_3a.py
+++ 09_functions/task_9_3a.py
@@ -23,3 +23,31 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+#!/usr/bin/env python3
+
+def get_int_vlan_map(config_filename):   
+    result = {}
+    result2 = {} 
+    pred_line=''#предыдущая строчка
+    with open(config_filename) as f:
+        for line in f:
+            if 'interface' in line:
+                interface = line.split()[1]
+            if 'access vlan' in line:
+                access = line.split()[-1]
+                result[interface] = access    
+            elif 'mode access' in pred_line and 'duplex' in line
+            #проверяет есть ли 'mode access и 'duplex' в двух строчках следующих друг за другом 
+                result[interface] = '1'
+            elif 'trunk allowed vlan' in line:
+                trunk = line.split()[-1]
+                result2[interface] = trunk
+            pred_line=line#предыдущая строчка следующая перед строчкой с 'duplex'
+    print(result)
+    print(result2)
+    return(result)
+    return(result2)
+get_int_vlan_map('config_sw2.txt')
+
+{'FastEthernet0/0': '10', 'FastEthernet0/2': '20', 'FastEthernet1/0': '20', 'FastEthernet1/1': '30', 'FastEthernet1/3': '1', 'FastEthernet2/0':'1', 'FastEthernet2/1': '1'}{'FastEthernet0/1': '100,200', 'FastEthernet0/3': '100,300,400,500,600', 'FastEthernet1/2': '400,500,600'}
