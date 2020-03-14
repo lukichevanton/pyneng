e701a58074e067fdae4c98eb0087a4a3a4d639bd
lukichevanton <lukichev_anton@mail.ru>
2020-03-14 14:22:50 +0000

    task_11_2.py

--- 11_modules/task_11_2.py
+++ 11_modules/task_11_2.py
@@ -70,7 +70,11 @@ def create_network_map(filenames):
                 local_remote[local_tuple] = remote_tuple#добавляет кортежи в словарь
     return(local_remote)#функция возвращает значение после прохождения цикла по всем строкам
 result = create_network_map(cdp)
-#print(result)
+
+'''
+{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'), ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'), ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'), ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'), ('R3', 'Eth0/1'): ('R4', 'Eth0/0'), ('R3', 'Eth0/2'): ('R5', 'Eth0/0'), ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'), ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'), ('SW1', 'Eth
+0/3'): ('R3', 'Eth0/0'), ('SW1', 'Eth0/5'): ('R6', 'Eth0/1')}
+'''
 
 result2 = {} 
 lists = [] 
