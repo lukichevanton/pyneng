8254d19b9dc1dfff8c6b455e0e5c4dcf3a11c6c8
lukichevanton <lukichev_anton@mail.ru>
2020-08-04 14:31:38 +0000

    task_18_3

--- 18_db/task_18_3/add_data.py
+++ 18_db/task_18_3/add_data.py
@@ -6,22 +6,20 @@ import sqlite3
 import yaml
 import glob
 import os
+from tabulate import tabulate
 
 dhcp_snooping_files = glob.glob("*dhcp_snooping.txt")#файлы c данными для dhcp_snooping.db#ищет все файлы в директории заканчивающиеся на dhcp_snooping.txt
 #print(dhcp_snooping_files)
 yml_filename = 'switches.yml'#файл c данными для dhcp_snooping.db
-
 db_filename = 'dhcp_snooping.db'#файл bd с данными из switches.yml и файлов *dhcp_snooping.txt
 db_exists = os.path.exists(db_filename)
+conn = sqlite3.connect(db_filename)
 
-if not db_exists:
-    print('База данных не существует. Перед добавлением данных, ее надо создать скриптом create_db.py')
-elif db_exists:
-    print('База данных существует')
-
-    conn = sqlite3.connect(db_filename)
-
-    def switches_dhcp(switches, dhcp):
+def switches_dhcp(switches, dhcp):
+    if not db_exists:
+        print('База данных не существует. Перед добавлением данных, ее надо создать скриптом create_db.py')
+    elif db_exists:
+        print('База данных существует')
 
         final_switches = []
         with open(switches) as f:
@@ -35,15 +33,11 @@ elif db_exists:
                         final_tuple = tuple(final2)
                     final_switches.append(final_tuple)
         #pprint(final_switches)
-
         print('Добавляю данные в таблицу switches...')
         for row in final_switches:
-            try:
-                with conn:
-                    query = '''insert into switches (hostname, location) values (?, ?)'''
-                    conn.execute(query, row)
-            except sqlite3.IntegrityError as e:
-                print('При добавлении данных: {} Возникла ошибка:'.format(row), e)
+            with conn:
+                query = '''REPLACE INTO switches VALUES (?, ?)'''#самое первое добавлении данных и в случае добаления одного и того 'hostname' обновляет все значения в полях 
+                conn.execute(query, row)
     
         final_dhcp = []
         regex = re.compile(r'(?P<mac>^\S+\d+) +'
@@ -69,32 +63,57 @@ elif db_exists:
                         final2_tuple = tuple(final2)
                         final_dhcp.append(final2_tuple)     
         #pprint(final_dhcp)
-
         print('Добавляю данные в таблицу dhcp...')
+        with conn:
+            query = '''UPDATE dhcp set active = '0' WHERE active = '1' '''
+            conn.execute(query)#перед началом каждого добавления данных проставляет 0 (ноль) в поле 'active'. Таким образом можно определить активность устройств.
         for row in final_dhcp:
-            try:
-                with conn:
-                    query = '''insert into dhcp (mac, ip, vlan, interface, switch) values (?, ?, ?, ?, ?)'''
-                    conn.execute(query, row)
-            except sqlite3.IntegrityError as e:
-                print('При добавлении данных: {} Возникла ошибка:'.format(row), e)
+            with conn:
+                query = '''REPLACE INTO dhcp VALUES (?, ?, ?, ?, ?, 1)'''
+                conn.execute(query, row)#при самом первом добавлении данных проставляет 1 (один) в поле 'active' и в случае добаления одного и того 'mac' обновляет все значения в полях
+
+        with conn:              
+            query = '''select * from dhcp '''
+            result = conn.execute(query)
+            print(tabulate(result))
+
         conn.close()
-    switches_dhcp(yml_filename, dhcp_snooping_files)
+switches_dhcp(yml_filename, dhcp_snooping_files)
 '''
-База данных не существует. Перед добавлением данных, ее надо 
-создать скриптом create_db.py
-
+Первое добавление данных:
 База данных существует
 Добавляю данные в таблицу switches...
 Добавляю данные в таблицу dhcp...
+-----------------  ---------------  --  ----------------  ---  -
+00:09:BB:3D:D6:58  10.1.10.2        10  FastEthernet0/1   sw1  1
+00:04:A3:3E:5B:69  10.1.5.2          5  FastEthernet0/10  sw1  1
+00:05:B3:7E:9B:60  10.1.5.4          5  FastEthernet0/9   sw1  1
+00:07:BC:3F:A6:50  10.1.10.6        10  FastEthernet0/3   sw1  1
+00:09:BC:3F:A6:50  192.168.100.100   1  FastEthernet0/7   sw1  1
+00:A9:BB:3D:D6:58  10.1.10.20       10  FastEthernet0/7   sw2  1
+00:B4:A3:3E:5B:69  10.1.5.20         5  FastEthernet0/5   sw2  1
+00:C5:B3:7E:9B:60  10.1.5.40         5  FastEthernet0/9   sw2  1
+00:A9:BC:3F:A6:50  10.1.10.60       20  FastEthernet0/2   sw2  1
+00:E9:BC:3F:A6:50  100.1.1.6         3  FastEthernet0/20  sw3  1
+00:E9:22:11:A6:50  100.1.1.7         3  FastEthernet0/21  sw3  1
+-----------------  ---------------  --  ----------------  ---  -
 
+Второе добавление/обновление данных:
 База данных существует
 Добавляю данные в таблицу switches...
-При добавлении данных: ('sw1', 'London, 21 New Globe Walk') Возникла ошибка: UNIQUE constraint failed: switches.hostname
-При добавлении данных: ('sw2', 'London, 21 New Globe Walk') Возникла ошибка: UNIQUE constraint failed: switches.hostname
-При добавлении данных: ('sw3', 'London, 21 New Globe Walk') Возникла ошибка: UNIQUE constraint failed: switches.hostname
 Добавляю данные в таблицу dhcp...
-При добавлении данных: ('00:09:BB:3D:D6:58', '10.1.10.2', '10', 'FastEthernet0/1', 'sw1') Возникла ошибка: UNIQUE constraint failed: dhcp.mac
-При добавлении данных: ('00:04:A3:3E:5B:69', '10.1.5.2', '5', 'FastEthernet0/10', 'sw1') Возникла ошибка: UNIQUE constraint failed: dhcp.mac
-При добавлении данных: ('00:05:B3:7E:9B:60', '10.1.5.4', '5', 'FastEthernet0/9', 'sw1') Возникла ошибка: UNIQUE constraint failed: dhcp.mac
+-----------------  ---------------  --  ----------------  ---  -
+00:09:BC:3F:A6:50  192.168.100.100   1  FastEthernet0/7   sw1  0
+00:C5:B3:7E:9B:60  10.1.5.40         5  FastEthernet0/9   sw2  0
+00:09:BB:3D:D6:58  10.1.10.2        10  FastEthernet0/1   sw1  1
+00:04:A3:3E:5B:69  10.1.15.2        15  FastEthernet0/15  sw1  1
+00:05:B3:7E:9B:60  10.1.5.4          5  FastEthernet0/9   sw1  1
+00:07:BC:3F:A6:50  10.1.10.6        10  FastEthernet0/5   sw1  1
+00:A9:BB:3D:D6:58  10.1.10.20       10  FastEthernet0/7   sw2  1
+00:B4:A3:3E:5B:69  10.1.5.20         5  FastEthernet0/5   sw2  1
+00:A9:BC:3F:A6:50  10.1.10.65       20  FastEthernet0/2   sw2  1
+00:A9:33:44:A6:50  10.1.10.77       10  FastEthernet0/4   sw2  1
+00:E9:BC:3F:A6:50  100.1.1.6         3  FastEthernet0/20  sw3  1
+00:E9:22:11:A6:50  100.1.1.7         3  FastEthernet0/21  sw3  1
+-----------------  ---------------  --  ----------------  ---  -
 '''
