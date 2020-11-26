56e27a07b4d8e2e8ee6891f09ff47c4d84177449
lukichevanton <lukichev_anton@mail.ru>
2020-11-26 15:07:29 +0000

    task_21_2.py

--- 21_textfsm/task_21_3.py
+++ 21_textfsm/task_21_3.py
@@ -18,3 +18,39 @@
 
 Проверить работу функции на примере вывода команды sh ip int br.
 """
+from netmiko import ConnectHandler
+import textfsm
+from tabulate import tabulate
+from pprint import pprint
+
+def parse_command_output(template, command_output):
+	final = []
+	with open(template) as template:
+		fsm = textfsm.TextFSM(template)
+		result = fsm.ParseText(command_output)
+		intf, address, status, protocol = fsm.header
+		for line in result:
+			final2 = {}
+			intf1, address1, status1, protocol1 = line
+			final2[intf] = intf1
+			final2[address] = address1
+			final2[status] = status1
+			final2[protocol] = protocol1
+			final.append(final2)
+	return final
+
+# вызов функции должен выглядеть так
+if __name__ == "__main__":
+    r1_params = {
+        "device_type": "cisco_ios",
+        "host": "ios-xe-mgmt.cisco.com",
+        "username": "developer",
+        "password": "C1sco12345",
+        "secret": "C1sco12345",
+        "port": "8181",
+    }
+    with ConnectHandler(**r1_params) as r1:
+        r1.enable()
+        output = r1.send_command("sh ip int br")
+    result = parse_command_output("templates/sh_ip_int_br.template", output)
+    pprint(result)
